1d79eb1ec7c65685fd9aceeae4ab856c4f1dbf68
Jana Ligotkina <janakolesnikova6293@gmail.com>
2024-02-22 14:08:02 +0400

    Refactor

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@
 
 require 'bundler/inline'
 require 'open-uri'
+require 'open3'
 
 gemfile do
   source 'https://rubygems.org'
@@ -10,19 +11,25 @@ gemfile do
 
   gem 'csv'
   gem 'nokogiri'
+  gem 'pastel'
   gem 'thor'
 end
 
-BASE_URL = 'https://www.petsonic.com/dermatitis-y-problemas-piel-para-perros/'.freeze
-CSV_FILENAME = 'pet_products.csv'.freeze
+module Xpath
+  PRODUCT_CONTAINER_XPATH = '//div[contains(@class, "product-container")]'.freeze
+  PRODUCT_URL_XPATH = './/a[@class="product_img_link pro_img_hover_scale product-list-category-img"]/@href'.freeze
+  BASE_NAME_XPATH = '//div[@itemtype="http://schema.org/Product"]/meta[@itemprop="name"]/@content'.freeze
+  IMAGES_XPATH = '//div[@itemtype="http://schema.org/Product"]/link[@itemprop="image"]/@href'.freeze
+  VARIANTS_XPATH = '//ul[@class="attribute_radio_list pundaline-variations"]/li'.freeze
+  VARIANT_NAME_XPATH = './/span[@class="radio_label"]'.freeze
+  PRICE_XPATH = './/span[@class="price_comb"]'.freeze
+end
 
-PRODUCT_CONTAINER_XPATH = '//div[contains(@class, "product-container")]'.freeze
-PRODUCT_URL_XPATH = './/a[@class="product_img_link pro_img_hover_scale product-list-category-img"]/@href'.freeze
-BASE_NAME_XPATH = '//div[@itemtype="http://schema.org/Product"]/meta[@itemprop="name"]/@content'.freeze
-IMAGES_XPATH = '//div[@itemtype="http://schema.org/Product"]/link[@itemprop="image"]/@href'.freeze
-VARIANTS_XPATH = '//ul[@class="attribute_radio_list pundaline-variations"]/li'.freeze
-VARIANT_NAME_XPATH = './/span[@class="radio_label"]'.freeze
-PRICE_XPATH = './/span[@class="price_comb"]'.freeze
+module ColorfulOutput
+  def pastel
+    @pastel ||= Pastel.new
+  end
+end
 
 class Product
   attr_accessor :name, :price, :image
@@ -47,12 +54,15 @@ class Category
   end
 end
 
-class Parser
-  def initialize
+class CategoryPageParser
+  include Xpath
+
+  def initialize(product_page_parser)
+    @product_page_parser = product_page_parser
     @page_number = 1
   end
 
-  def parse_category_page(category)
+  def parse(category)
     category.products.clear
     sleep(1)
     open_url = URI.open(category.url)
@@ -63,15 +73,19 @@ class Parser
 
     doc.xpath(PRODUCT_CONTAINER_XPATH).each do |product|
       product_url = product.xpath(PRODUCT_URL_XPATH).text.strip
-      parse_product_page(product_url).each do |product|
+      @product_page_parser.parse(product_url).each do |product|
         category.add_product(product)
       end
     end
 
     category.products.empty? ? nil : category.products
   end
+end
 
-  def parse_product_page(url)
+class ProductPageParser
+  include Xpath
+
+  def parse(url)
     doc = Nokogiri::HTML(URI.open(url))
 
     base_name = doc.xpath(BASE_NAME_XPATH).text
@@ -89,22 +103,31 @@ class Parser
 
     variants
   end
+end
 
-  def write_to_csv(category, filename)
+class CsvWriter
+  def write(category, filename)
     CSV.open(filename, 'a') do |csv|
       category.products.each do |product|
         csv << [product.name, product.price, product.image]
       end
     end
   end
+end
 
-  def record(category_url, filename)
-    category = Category.new(category_url)
+class Parser
+  def initialize(category_page_parser, product_page_parser, csv_writer)
+    @category_page_parser = category_page_parser
+    @product_page_parser = product_page_parser
+    @csv_writer = csv_writer
+    @page_number = 1
+  end
 
+  def record_to_file(category, category_url, filename)
     loop do
-      break if parse_category_page(category).nil?
+      break if @category_page_parser.parse(category).nil?
 
-      write_to_csv(category, filename)
+      @csv_writer.write(category, filename)
 
       @page_number += 1
       category.url = "#{category_url}?p=#{@page_number}"
@@ -115,15 +138,29 @@ end
 class ProductParserCLI < Thor
   # Example start:
   # ruby main.rb record "https://www.petsonic.com/dermatitis-y-problemas-piel-para-perros/" "pet_products.csv"
+  include ColorfulOutput
   desc 'record URL FILENAME', 'Process a category page and write the results to a file'
   def record(url, filename)
-    parser = Parser.new
+    say pastel.yellow("Picking up products from: `#{url}`")
+    say pastel.yellow("Creating a file with the name `#{filename}`")
 
+    create_file(filename)
+    category = Category.new(url)
+    product_page_parser = ProductPageParser.new
+    category_page_parser = CategoryPageParser.new(product_page_parser)
+    csv_writer = CsvWriter.new
+    parser = Parser.new(category_page_parser, product_page_parser, csv_writer)
+    parser.record_to_file(category, url, filename)
+
+    say pastel.bold.green('Done!')
+  end
+
+  private
+
+  def create_file(filename)
     CSV.open(filename, 'wb') do |csv|
       csv << %w[Name Price Image]
     end
-
-    parser.record(url, filename)
   end
 end
 
